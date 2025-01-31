944bfbc6e086fb5ca7217c04f6c412856fa695a6
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-01-31 09:16:31 +0530

    hello world

---
+++ old-react-app/hello-react-app/src/App.js
@@ -0,0 +1,11 @@
+import Hello from "./Components/hello";
+
+function App() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ old-react-app/hello-react-app/src/Components/hello.jsx
@@ -0,0 +1,9 @@
+function Hello() {
+  return (
+    <div>
+      <h1>helloworld. react js.</h1>
+    </div>
+  );
+}
+
+export default Hello;
